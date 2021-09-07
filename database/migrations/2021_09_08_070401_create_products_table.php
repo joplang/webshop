ba130d783c3add2c6645d0ada2a4a4d37daa0416
BasLeeuwerik <bas_leeuwerik@hotmail.com>
2021-09-07 11:22:15 +0200

    products and artists

---
+++ database/migrations/2021_09_08_070401_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('artist_id');
+            $table->foreign('artist_id')->references('id')->on('artists');
+            $table->unsignedBigInteger('label_id');
+            $table->foreign('label_id')->references('id')->on('labels');
+            $table->unsignedBigInteger('genre_id');
+            $table->foreign('genre_id')->references('id')->on('genres');
+            $table->string('title');
+            $table->year('year');
+            $table->text('description');
+            $table->text('track_list');
+            $table->string('img');
+            $table->float('price');
+            $table->userstamps();
+            $table->softUserstamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
