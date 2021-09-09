f710790ef31e8e1bf74ac8b8e0896bf46d3c156a
joplang <joplang@hotmail.com>
2021-09-09 14:14:23 +0200

    navbar waar hij hoort

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,3 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/product', [\App\Http\Controllers\ProductController::class, 'index'])->name('product');
