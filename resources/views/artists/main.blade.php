b5c4c1889a83df31b8133a20fc7dcd25e47cb633
Femke Petter <fnpetter@gmail.com>
2021-09-14 13:14:15 +0200

    artiesten weergave\

--- resources/views/artists/main.blade.php
+++ resources/views/artists/main.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-
+ <div class="container-fluid">
+     @foreach ($artists as $artist)
+         <div class="card">
+             <a href="/artists/{{ $artist->id }}">
+                 <div class="card-title"> {{ $artist->name }} </div>
+             </a>
+         </div>
+     @endforeach
+ </div>
 
 @endsection
