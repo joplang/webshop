43758c8fce40291acfd04ec6bd4d915171d5c0a4
Femke Petter <fnpetter@gmail.com>
2021-09-20 11:39:26 +0200

    informatie homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,38 +22,38 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Home</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/products">Albums</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/artists">Artiesten</a>
-        </li>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Home</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-          <a class="nav-link" href="/labels">Labels</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/genres">Genres</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/discounts">Aanbiedingen!</a>
-        </li>
-        <li class="nav-item">
-          <a class="btn btn-success" href="/cart">
-            <i class="fa fa-shopping-cart"></i>Winkelmand
-              <span class="badge badge-light">3</span>
-          </a>
-        </li>
-              <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+                  <a class="nav-link active" aria-current="page" href="/products">Albums</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/artists">Artiesten</a>
+                </li>
+                        <li class="nav-item">
+                  <a class="nav-link" href="/labels">Labels</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/genres">Genres</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/discounts">Aanbiedingen!</a>
+                </li>
+                <li class="nav-item">
+                  <a class="btn btn-success" href="/cart">
+                    <i class="fa fa-shopping-cart"></i>Winkelmand
+                      <span class="badge badge-light">3</span>
+                  </a>
+                </li>
+                      <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
              <ul class="navbar-nav">
                         @guest
                             @if (Route::has('login'))
@@ -87,9 +87,9 @@
                             </li>
                         @endguest
                     </ul>
-    </div>
-  </div>
-</nav>
+            </div>
+          </div>
+        </nav>
         <main class="py-4">
             @yield('content')
         </main>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,46 +24,70 @@
     -->
 @extends('layouts.app')
 @section('content')
+<span class="d-block bg-dark text-white text-center">  
+    <h2>Het is Power Metal-week!</h2>
+    <p>De hele week 50.50% korting op alle <a href="/labels/1">Power Metal</a></p>
+</span>
+
 
 <div class="container-sm" style="width: 50rem;">
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="https://images.unsplash.com/photo-1542208998-f6dbbb27a72f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="card">
+                <b>  <h4>Albums van de maand!</h4> </b>
+                <p>Dit zijn onze uitgelichte albums!</p>
+              </div>
+            </div>
+       </div>
+      @foreach ($highlights as $highlight)
+        <div class="carousel-item">
+          <img src="https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="card">
+                <a href="/products/{{$highlight->id}}"> <h4>{{$highlight->album_title}}</h4> </a>
+                <p>{{$highlight->track_list}}.</p>
+                <h6> €{{$highlight->price}}</h6>
+              </div>
+          </div>
+       </div>
+      @endforeach
+      </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
-    <div class="carousel-item">
-      <img src="https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="https://images.unsplash.com/photo-1459749411175-04bf5292ceea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
 </div>
+<div class="container-sm p-2">
+  <div class="row">
+    <div class="col-6">
+      <div class="card">
+        <div class="card-header">Onze genres!</div>
+        <ul class="list-group list-group-flush">
+          @foreach ($genres as $genre)
+            <li class="list-group-item"><a href="/labels/{{$genre->id}}">{{$genre->name}}</a></li>
+           @endforeach
+        </div>
+      </div>
+    <div class="col-6">
+      <div class="card">
+        <div class="card-header">Platenlabels!</div>
+        <ul class="list-group list-group-flush">
+          @foreach ($labels as $label)
+          <li class="list-group-item"><a href="/labels/{{$label->id}}">{{$label->name}}</a></li>
+         @endforeach
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
   </body>
